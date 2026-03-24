bb50e16e8b30abe8bd0fb18f1a775e8a920ae536
Kim Valkama <hello@valkama.kim>
2026-03-24 17:25:48 +0200

    Removed lualine from avante sidebar

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -3,5 +3,26 @@ require('lualine').setup({
     theme = 'tokyonight',
     component_separators = { left = '', right = ''},
     section_separators = { left = '', right = ''},
-  }
+
+    disabled_filetypes = {
+      statusline = {
+        "Avante",
+        "AvanteInput",
+        "AvanteSidebar",
+      },
+      winbar = {
+        "Avante",
+        "AvanteInput",
+        "AvanteSidebar",
+      },
+    },
+  },
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "Avante", "AvanteInput", "AvanteSidebar" },
+  callback = function()
+    vim.opt_local.winbar = nil
+    vim.opt_local.statusline = ""
+  end,
 })
